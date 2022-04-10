d0d8b53d1687d734668ccad8c1b8b1ffa248672e
VGFP <pierwiastekz3@interia.pl>
2022-04-10 19:12:45 +0200

    dataloder fix

--- src/dataloader.py
+++ src/dataloader.py
@@ -14,17 +14,17 @@ class Dataloader:
         self.features = None
         self.data_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..',  'dataset'))
         self.data_files = ['UNSW-NB15_1.csv', 'UNSW-NB15_2.csv', 'UNSW-NB15_3.csv'] # There is an issue with the 4th file, it is not in the correct format 'UNSW-NB15_4.csv'
-        self.labels_file = 'NUSW-NB15_features.csv'
+        self.labels_file = 'NUSW-NB15_features_v2.csv'
 
     def load_data(self):
         self.data = []
         for file in self.data_files:
             print(f'Loading data from file: {file}')
             self.data.append(pd.read_csv(os.path.join(self.data_path, file), header=None))
-        flat_data = self.data.values.flatten()
-        # for data in self.data:
-        #     flat_data.append(data.values.flatten())
-        self.data = flat_data
+        with open(os.path.join(self.data_path, self.labels_file), 'r') as f:
+            self.labels = f.readlines()
+        self.data = np.array(self.data)
+        self.data = np.reshape(self.data, (700001*3, 49))
         self.df = pd.DataFrame(self.data)
 
     def get_data(self):
